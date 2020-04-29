ccb4a2df6ba398ed5fc8f94456543c5f96216348
Andrew Black <andrew@black1.org.uk>
2020-04-29 15:29:52 +0100

    first pass

--- zoomid.pl
+++ zoomid.pl
@@ -1,4 +1,20 @@
+=head 1
+
+Topic: St John's East Dulwich's Coffee Morning
+Time: Apr 29, 2020 11:00 AM London
+
+Join Zoom Meeting
+https://us02web.zoom.us/j/87048507656
+
+Meeting ID: 870 4850 7656
+
+-cut
+
+
 while (<>)
 {
-
+    if ( /Topic:|Time:/ ) {
+    	print;
+    	next;
+    }
 }
