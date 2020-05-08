4a0525c5dabfb569412ac7a4eec966f2e60744b8
Andrew Black <andrew@black1.org.uk>
2020-05-09 00:24:49 +0100

    add print

--- zoomid.pl
+++ zoomid.pl
@@ -1,4 +1,16 @@
-=head 1
+#!/usr/bin/perl -w
+
+=head1 NAME
+
+**file - **desc
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 TO DO
+
+=head1 SAMPLE
 
 Topic: St John's East Dulwich's Coffee Morning
 Time: Apr 29, 2020 11:00 AM London
@@ -8,13 +20,25 @@ https://us02web.zoom.us/j/87048507656
 
 Meeting ID: 870 4850 7656
 
--cut
 
+=cut
 
+use strict ;
+use warnings ;
+use feature ":5.10" ;
+use Data::Dumper ;
+
+say '' ;
 while (<>)
 {
-    if ( /Topic:|Time:/ ) {
+     if ( /Topic:|Time:/ ) {
     	print;
     	next;
     }
+   if ( /Meeting ID:|https/ ) {
+   	    say '' ;
+    	print;
+    	next;
+    }
+
 }
