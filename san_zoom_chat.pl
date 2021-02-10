520eb6dac26f2a941b4424097ffac4e939daf795
Andrew Black <andrew@black1.org.uk>
2021-02-10 15:34:39 +0000

    first vaguelly working

--- san_zoom_chat.pl
+++ san_zoom_chat.pl
@@ -6,6 +6,8 @@ xxxxx.pl -
 
 =head1 SYNOPSIS
 
+11:06:19 From Andrew Black To Everyone : text .......
+
 =head1 DESCRIPTION
 
 =head1 TO DO
@@ -18,3 +20,9 @@ use 5.10.0 ;
 use Data::Dumper ;
 use File::Spec ;
 
+while (<>) {
+  # will fail if someone has : in name or if text has :
+  s/(\d\d:\d\d:\d\d \s*) ([^:]*) ://x ;
+  # TODO save time etc
+  print ;
+}
