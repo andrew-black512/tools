4077fd53442fd1010a7a1968d0518bd237caa538
Andrew Black <andrew@black1.org.uk>
2021-02-10 15:25:26 +0000

    new file:   san_zoom_chat.pl

---
+++ san_zoom_chat.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/perl -w
+
+=head1 NAME
+
+xxxxx.pl -
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 TO DO
+
+=cut
+
+use strict ;
+use warnings ;
+use 5.10.0 ;
+use Data::Dumper ;
+use File::Spec ;
+
