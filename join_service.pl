d8c30195d36ccc1e9e229762b72c9dba127b716d
Andrew Black <andrew@black1.org.uk>
2021-02-21 11:30:40 +0000

    add cc (wrong it turns out)

--- join_service.pl
+++ join_service.pl
@@ -1,6 +1,8 @@
 
 my %services =(
      s => 'https://www.facebook.com/southwarkcathedral' ,
+     sj => 'https://www.youtube.com/channel/UC76aEaiHelFz-F8QAmOS0tw' ,
+     cc => 'https://www.youtube.com/channel/UCfh-pjC5b9PTS-Svf-JIn6w/videos' ,
   ) ;
 my $choice = shift ;
 my $chosenservice = $services { $choice } ;
