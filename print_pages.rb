3e0932ff21847490f299636cd40650c3ffee17c9
Andrew Black <andrew@black1.org.uk>
2021-07-12 16:22:28 +0100

    first attempt at comma_join

--- print_pages.rb
+++ print_pages.rb
@@ -1,5 +1,22 @@
 #!/usr/bin/ruby
 
+#---------------------------------------------------------------------
+#potential lib
+class Array
+  def comma_join( arr )
+    arr.join(',')
+  end
+end
+def comma_join_test
+  puts ['a'].comma_join
+  #  ) # %w/a/ )
+end
+
+#---------------------------------------------------------------------
+
 start_page = ARGV.shift
 end_page = ARGV.shift
+## TODO: check arg (raise error)
 puts "#{start_page} #{end_page} "
+
+comma_join_test
