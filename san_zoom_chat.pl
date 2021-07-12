b3d9f1612d7a10787a1e794e979f115abb2f1ea7
Andrew Black <andrew@black1.org.uk>
2021-07-12 23:50:46 +0100

    various - see diff

--- san_zoom_chat.pl
+++ san_zoom_chat.pl
@@ -6,6 +6,7 @@ xxxxx.pl -
 
 =head1 SYNOPSIS
 
+Format : 
 11:06:19 From Andrew Black To Everyone : text .......
 
 =head1 DESCRIPTION
@@ -20,9 +21,11 @@ use 5.10.0 ;
 use Data::Dumper ;
 use File::Spec ;
 
+say "see also - suspect format has changed" ;
+
 while (<>) {
   # will fail if someone has : in name or if text has :
-  s/(\d\d:\d\d:\d\d \s*) ([^:]*) ://x ;
+  s/(\d\d:\d\d:\d\d \s*) ([^:]*) : \s //x ;
   # TODO save time etc
   print ;
 }
