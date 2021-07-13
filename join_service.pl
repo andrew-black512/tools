c5c3671e2c3b7a481c698e1090b41b1ad78aa85b
Andrew Black <andrew@black1.org.uk>
2021-07-13 01:51:08 +0100

    add CC urls

--- join_service.pl
+++ join_service.pl
@@ -2,7 +2,9 @@
 my %services =(
      s => 'https://www.facebook.com/southwarkcathedral' ,
      sj => 'https://www.youtube.com/channel/UC76aEaiHelFz-F8QAmOS0tw' ,
-     cc => 'https://www.youtube.com/channel/UCfh-pjC5b9PTS-Svf-JIn6w/videos' ,
+     ccw => 'https://www.youtube.com/channel/UCfh-pjC5b9PTS-Svf-JIn6w/videos' ,
+     ccs => 'https://vimeo.com/stbarnabasdulwich/videos' ,
+     roch-off => 'https://www.rochestercathedral.org/worship-online'
   ) ;
 my $choice = shift ;
 my $chosenservice = $services { $choice } ;
