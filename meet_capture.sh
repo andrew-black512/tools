aa6c7955895a512ffd609641a53218f8923d6fa9
Andrew Black <andrew@black1.org.uk>
2021-07-13 01:53:13 +0100

    see diffs ...

--- meet_capture.sh
+++ meet_capture.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
+#TODO - if no argument - default or refuse. Could be used from a keypress with no change to add name
+
 NAME=$1
-FILE=~/work/meetings/$NAME
+FILE=$MEET/$NAME
+
+
+
+echo ======================== >> $FILE
+echo "   Saved" $( date +"%a %b %d") >> $FILE
 
 xclip -o >> $FILE
 echo >> $FILE
