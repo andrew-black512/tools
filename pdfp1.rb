fe59b653bac856b388a1f97037effac40990b9f6
Andrew Black <andrew@black1.org.uk>
2021-07-19 17:11:59 +0100

    about to get "unite"

--- pdfp1.rb
+++ pdfp1.rb
@@ -1,12 +1,20 @@
 #!/usr/bin/ruby
 
+exit if ARGV.count < 2 
+
 fileglob = ARGV.shift
+outfile = ARGV.shift
+
 partlist = []
 Dir.glob(fileglob).each do |file|
    puts file
     #TODO \" isnt getting throuhg. Not needed at moment
     partfile = "#{file}_part"
+    system "pdfinfo  #{partfile} | grep Pages  "
     system "pdfseparate -l 1 #{file}  #{partfile}  "
+
     partlist.push partfile
 end
+puts ''
 puts partlist.join ' '
+system "echo pdfunite   #{partlist.join ' '}  #{outfile} "
