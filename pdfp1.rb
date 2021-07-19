d4776503da50ba0359efae76e79c44e3a4c8631a
Andrew Black <andrew@black1.org.uk>
2021-07-19 17:04:20 +0100

    flesh out

--- pdfp1.rb
+++ pdfp1.rb
@@ -1,1 +1,12 @@
 #!/usr/bin/ruby
+
+fileglob = ARGV.shift
+partlist = []
+Dir.glob(fileglob).each do |file|
+   puts file
+    #TODO \" isnt getting throuhg. Not needed at moment
+    partfile = "#{file}_part"
+    system "pdfseparate -l 1 #{file}  #{partfile}  "
+    partlist.push partfile
+end
+puts partlist.join ' '
