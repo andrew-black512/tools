90057dbd170f8f919b0576c3c90001edd1c86cb3
Andrew Black <andrew@black1.org.uk>
2021-08-03 21:59:54 +0100

    print + lines

--- san_diff.rb
+++ san_diff.rb
@@ -1,15 +1,21 @@
 #!/usr/bin/ruby
 
-## quick demo of how ARGF ( perl <> ) works
+
+def process_diff ( line )
+
+  case line
+    when /^\+/
+      puts "  add #{line}"
+  end
+
+end
 combined_argv=ARGV.join '-'
-pp ARGV
-puts "ARGV=#{ combined_argv } "
 indent = '    '
 
 
 
 #TODO - end/ change of file
 ARGF.each do |line|
-    puts "#{indent}#{line}"
+    process_diff line
 
 end
