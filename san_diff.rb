34e4a64b49a1f0106e65ebb54d44a32c3f2811b1
Andrew Black <andrew@black1.org.uk>
2021-08-04 10:23:23 +0100

    only fields currently of interest

--- san_diff.rb
+++ san_diff.rb
@@ -8,25 +8,30 @@ def git_date2txt( datestring)
   # Date:   Mon Aug 2 14:39:56 2021 +0100
   # assume we dont care about seconds, tz or year most of time.
   wd,month,day,time,year = datestring.split
-  return sprintf "%3s-%02s", month, day
+  return sprintf "%02d-%3s %s", day, month, time[0,5]
 end
 def process_diff ( line )
 
 
   case line
   when /^commit\s+(.*)/
+    puts ''
     puts "Commit   #{$1}"
   when /^Author:\s+(.*)/
-    puts "   #{$1}"
+    # At moment this is only me so boring.
+    #puts "   #{$1}"
   when /^Date:  \s+(.*)/
     datestring = git_date2txt $1
     puts "   #{datestring}"
 
     when /^\+{3} b\/([.\w]+)/
       file = $1
-      puts "  File #{file}"
+      puts "   #{file}"
     when /^\+/
       puts "     #{line}"
+    when /^\s+(.+)/
+      logline = $1
+      puts "     #{logline}"
   end
 
 end
