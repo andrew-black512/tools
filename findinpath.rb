8e34ba35be765d6da8731a21d545689c449b6d6b
Andrew Black <andrew@black1.org.uk>
2021-08-12 20:20:16 +0100

    flesh out findinpath.rb

--- findinpath.rb
+++ findinpath.rb
@@ -1,1 +1,17 @@
 #!/usr/bin/ruby
+require_relative  'lib/different.rb'
+wanted = ARGV.shift
+location = ENV['PATH']
+wanted_glob = "*#{wanted}*"
+
+dif = Different.new
+dif.different '' # force first time
+
+location.split(':').each do |dirname|
+   Dir.glob( wanted_glob,  base: dirname).each do |file|
+     puts dirname if dif.different dirname
+     puts "    #{file}"
+
+   end
+
+end
