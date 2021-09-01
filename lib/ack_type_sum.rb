a73ada0c7a5d2cd12c1d95df496eca248cba8496
Andrew Black <andrew@black1.org.uk>
2021-09-01 18:22:35 +0100

    flesh out

--- lib/ack_type_sum.rb
+++ lib/ack_type_sum.rb
@@ -1,1 +1,18 @@
 #!/usr/bin/ruby
+require 'pry'
+
+class AckType
+  def self.get_type_array ( directory )
+    types_string = `ack -f --show-types #{directory}`
+    pp types_string
+    (types_string.split"\n" )
+     .map{|x| x.split(' => ')}
+     .group_by{|y| y[1]}
+
+
+
+  end
+end
+
+s = AckType.get_type_array ()
+binding.pry
