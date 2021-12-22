20bd55298d0448e69ee6a2c755e2d906b0e42d3c
Andrew Black <andrew@black1.org.uk>
2021-12-22 14:33:27 +0000

    add extra urls and checks

--- chrome.pl
+++ chrome.pl
@@ -18,9 +18,15 @@ use 5.10.0 ;
 use Data::Dumper ;
 use File::Spec ;
 
-my %sites = {
+my %sites = (
   g => 'www.google.com' ,
-} ;
+  c => 'www.thecalmzone.net/help/webchat/' ,
+  rec => 'drive.google.com/drive/folders/0BybtFQ3kyI3RZl9DenRjSDFLTVU' ,
+  ph => 'docs.google.com/document/d/192faOiM0Zso9LxjvT0CeZnC0FyUrXbwjVk0IqKahZ-o' ,
+) ;
 my $verb = shift ;
 my $site = $sites {$verb} ;
+say "verb=$verb site=$site" ;
+die unless $site ;
+
 system "chromium-browser --app=https://$site"
