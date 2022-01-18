36683f8204d87d0ba3c8dd97323ab145b6cf29fe
Andrew Black <andrew@black1.org.uk>
2022-01-18 20:25:22 +0000

    change to use "we" "you" hueristics

--- calm.pl
+++ calm.pl
@@ -25,6 +25,7 @@ my %prefix = (
 say "Calm output" ;
 my $direction = 0 ; # initially don't print
 while (<>) {
+    #These affect following line
     if (/CALM Helpline/ || /helpline member/)  {
        $direction = 1 ;
        #print Dumper $direction ;
@@ -33,6 +34,11 @@ while (<>) {
     next if /^$/ ;
     next if /^\d\d:\d\d/ ; # TODO preserve time
     s/'/''/g ;  # apostrophes mess up the colouring (bodge)
+
+    # I rarely use WE or YOU but they do... 
+    if ( /\bwe\b/ix || /\b you r? \b/ix ) {
+      $direction = 1 ;
+    }
 
     if ($direction) {
       print $prefix{ $direction } . $_ ;
