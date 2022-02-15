b9fc24213a701828d9983e319b0524c83f3bf886
Andrew Black <andrew@black1.org.uk>
2022-02-15 10:59:22 +0000

    add flick to exception

--- chrome.pl
+++ chrome.pl
@@ -19,12 +19,16 @@ use Data::Dumper ;
 use File::Spec ;
 
 my %sites = (
+  fg => 'www.flickr.com/groups/plot94/' ,
+  fr => 'www.flickr.com/cameraroll/' ,
   g => 'www.google.com' ,
   c => 'www.thecalmzone.net/help/webchat/' ,
   ch => 'mail.google.com/chat/u/0/#chat/dm/tAjbzgAAAAE' ,
+  l  => 'docs.google.com/spreadsheets/d/1lVMNAgCsiAaKcMG3sTG-d4XHi_Fod97I0IhqLxz7ssY' ,
   m => 'mail.google.com/mail/u/0/#label/512Blank' ,
   rec => 'drive.google.com/drive/folders/0BybtFQ3kyI3RZl9DenRjSDFLTVU' ,
   ph => 'docs.google.com/document/d/192faOiM0Zso9LxjvT0CeZnC0FyUrXbwjVk0IqKahZ-o' ,
+  track => 'drive.google.com/drive/folders/1ofvnef_DgEeNbXM9hlih2btEPENrTN54' ,
 ) ;
 my $verb = shift ;
 my $site = $sites {$verb} ;
@@ -32,7 +36,7 @@ say "verb=$verb site=$site" ;
 die unless $site ;
 
 my $command='chromium-browser --app=https://%s' ;
-if ($site =~ /folders/) {
+if ($site =~ /folders|flickr/) {
   # folders in drive are better in a window with tabs.
   $command='chromium-browser %s' ;
 
