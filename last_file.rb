01918e912868e0f20df9de7086eaf1bca870cac5
Andrew Black <andrew@black1.org.uk>
2022-04-19 14:55:51 +0100

    diags to STDERR

--- last_file.rb
+++ last_file.rb
@@ -1,7 +1,13 @@
 #!/usr/bin/ruby
 
-dirglob = ARGV.shift || '[a-z]*[0-9]'
+dirglob = ARGV.shift || '[a-z]*[0-9]*'
 #puts "Glob:" + dirglob
-dirs = Dir.glob( dirglob ).sort
-#puts "Dirs:" + dirs.join(' ')
-puts dirs.last  # for CD
+dirs = Dir.glob( dirglob )
+#
+
+
+
+STDERR.puts "Dirs:" + dirs.join(' ')
+chosen=dirs.max
+STDERR.puts "Chosen:"+chosen
+puts chosen  # for CD
