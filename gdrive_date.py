b6783e1a81c72ce04408d09f429c9211983d4f55
Andrew Black <andrew@black1.org.uk>
2022-05-19 13:20:20 +0100

    test arg 1 or 2 (half cocked)

--- gdrive_date.py
+++ gdrive_date.py
@@ -3,12 +3,20 @@ import argparse
 import sys
 from datetime import timedelta,datetime
 
-earlier = int(sys.argv[1])
-later = int(sys.argv[2])
+# abs makes -n same as n (as in find -mtime)
+earlier = abs(int(sys.argv[1]))
+if len(sys.argv)-1 >= 2:
+    print('2 args')
+    later = abs(int(sys.argv[2]))
+else:
+    later = -1
 now = datetime.now()
 
 earlier_date = now - timedelta(earlier)
 et = earlier_date.strftime("after:%Y-%m-%d ")
+#if ??????? 
 later_date = now - timedelta(later)
 lt = later_date.strftime("before:%Y-%m-%d ")
+lt = ''
+
 print (et+lt)
