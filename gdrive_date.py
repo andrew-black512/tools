b29df7429634c78d8aa6aa9d869153882ba9caa7
Andrew Black <andrew@black1.org.uk>
2022-05-19 11:30:35 +0100

    Add later time

--- gdrive_date.py
+++ gdrive_date.py
@@ -4,7 +4,11 @@ import sys
 from datetime import timedelta,datetime
 
 earlier = int(sys.argv[1])
+later = int(sys.argv[2])
 now = datetime.now()
+
 earlier_date = now - timedelta(earlier)
-print (earlier_date.strftime("after:%Y-%m-%d") )
-#print (x)
+et = earlier_date.strftime("after:%Y-%m-%d ")
+later_date = now - timedelta(later)
+lt = later_date.strftime("before:%Y-%m-%d ")
+print (et+lt)
