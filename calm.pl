296642504c2df4bc1043c82abf6e30518e27fbdc
Andrew Black <andrew@black1.org.uk>
2022-12-29 15:23:20 +0000

    add merge stuff

--- calm.pl
+++ calm.pl
@@ -17,6 +17,26 @@ use warnings ;
 use feature ":5.10" ;
 use Data::Dumper ;
 
+#--------
+my $lastline ;
+sub merge_in {
+    my $fh=shift ;
+    my $time=shift ;
+    say "Merge in $time" ;
+    $lastline = <$fh> unless $lastline ;
+    #say $lastline ;
+    while(  $lastline lt $time )
+    {
+        print $lastline  ;
+        $lastline = <$fh>
+    }
+}
+#--------
+my $merge_file = shift ;  # ARG1 
+open(my $FH, "<", $merge_file)
+                    or die "Can't open '$merge_file' : $!" ;
+
+
 my %prefix = (
    1 => '# '    ,       # Them
   -1 => '     ' ,       # Me
@@ -32,7 +52,10 @@ while (<>) {
        next ;
     }
     next if /^$/ ;
-    next if /^\d\d:\d\d/ ; # TODO preserve time
+    if (/^ ( \d\d:\d\d ) /x ) {
+        merge_in $FH, $1 ;
+        next ;
+    }
     s/'/''/g ;  # apostrophes mess up the colouring (bodge)
 
     # I rarely use WE or YOU but they do...
