31e9506ed9a858416ebed443978f1fe6982fb41b
Andrew Black <andrew@black1.org.uk>
2023-03-10 15:50:10 +0000

    misc

--- wifip.rb
+++ wifip.rb
@@ -1,1 +1,8 @@
 #!/usr/bin/ruby
+# TODO:  Ouput to a well known file 
+require "clipboard"
+
+password = ARGV.last 
+fileoutput = ARGV.join(' ') 
+Clipboard.copy password
+puts "Clipboard : #{Clipboard.paste}"
