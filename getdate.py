09cf12d884af70d1258cb253cd096a2171fdb267
Andrew Black <andrew@black1.org.uk>
2023-11-21 13:58:33 +0000

    remove lt code

--- getdate.py
+++ getdate.py
@@ -6,20 +6,13 @@ from datetime import timedelta,datetime
 # abs makes -n same as n (as in find -mtime)
 numargs = len(sys.argv)-1
 if numargs >= 1:
-    earlier = abs(int(sys.argv[1]))
+    earlier =  int(sys.argv[1])
 else:
-    earlier = 7  ### prefer to say None or nil
-if numargs >= 2:
-    later = abs(int(sys.argv[2]))
-else:
-    later = -1  ### prefer to say None or nil
+    earlier = 0 
+
 now = datetime.now()
 
 earlier_date = now - timedelta(earlier)
-et = earlier_date.strftime("after:%Y-%m-%d ")
-#if ???????
-later_date = now - timedelta(later)
-lt = later_date.strftime("before:%Y-%m-%d ")
-lt = ''
+et = earlier_date.strftime("%Y-%m-%d ")
 
-print (et+lt)
+print (et)
