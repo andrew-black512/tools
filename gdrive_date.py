a68781ef7342bd8efd1bd1aa090f8a2027208816
Andrew Black <andrew@black1.org.uk>
2023-11-21 13:42:03 +0000

    attempt to make param

--- gdrive_date.py
+++ gdrive_date.py
@@ -4,17 +4,20 @@ import sys
 from datetime import timedelta,datetime
 
 # abs makes -n same as n (as in find -mtime)
-earlier = abs(int(sys.argv[1]))
-if len(sys.argv)-1 >= 2:
-    print('2 args')
+numargs = len(sys.argv)-1
+if numargs >= 1:
+    earlier = abs(int(sys.argv[1]))
+else:
+    earlier = 7  ### prefer to say None or nil
+if numargs >= 2:
     later = abs(int(sys.argv[2]))
 else:
-    later = -1
+    later = -1  ### prefer to say None or nil
 now = datetime.now()
 
 earlier_date = now - timedelta(earlier)
 et = earlier_date.strftime("after:%Y-%m-%d ")
-#if ??????? 
+#if ???????
 later_date = now - timedelta(later)
 lt = later_date.strftime("before:%Y-%m-%d ")
 lt = ''
