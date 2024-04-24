100017f2da055d29c6ac57b330bae87c0e9673ce
Andrew Black <andrew@black1.org.uk>
2024-04-24 14:39:09 +0100

    date going forwards

--- file_date.py
+++ file_date.py
@@ -6,21 +6,15 @@ from datetime import timedelta,datetime
 # abs makes -n same as n (as in find -mtime)
 numargs = len(sys.argv)-1
 if numargs >= 1:
-    earlier = abs(int(sys.argv[1]))
+    #number of days forward - default today
+    earlier = int(sys.argv[1])
 else:
     earlier = 0  ### prefer to say None or nil
-if numargs >= 2:
-    later = abs(int(sys.argv[2]))
-else:
-    later = -1  ### prefer to say None or nil
 now = datetime.now()
 prefix="DF "   # TODO
 
-earlier_date = now - timedelta(earlier)
+earlier_date = now + timedelta(earlier)
 et = earlier_date.strftime("DF %Y-%m-%d ")
 #if ???????
-later_date = now - timedelta(later)
-lt = later_date.strftime("before:%Y-%m-%d ")
-lt = ''
 
-print (et+lt)
+print (et)
