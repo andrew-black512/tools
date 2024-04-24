9b8524ba3d93aacb186ecacde0810b956e313ad1
Andrew Black <andrew@black1.org.uk>
2024-04-24 14:32:59 +0100

    Start to produce file date...

--- file_date.py
+++ file_date.py
@@ -8,15 +8,16 @@ numargs = len(sys.argv)-1
 if numargs >= 1:
     earlier = abs(int(sys.argv[1]))
 else:
-    earlier = 7  ### prefer to say None or nil
+    earlier = 0  ### prefer to say None or nil
 if numargs >= 2:
     later = abs(int(sys.argv[2]))
 else:
     later = -1  ### prefer to say None or nil
 now = datetime.now()
+prefix="DF "   # TODO
 
 earlier_date = now - timedelta(earlier)
-et = earlier_date.strftime("after:%Y-%m-%d ")
+et = earlier_date.strftime("DF %Y-%m-%d ")
 #if ???????
 later_date = now - timedelta(later)
 lt = later_date.strftime("before:%Y-%m-%d ")
