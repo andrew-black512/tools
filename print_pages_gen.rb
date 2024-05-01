49610e203ddb1155d6dfbdf42b8ad0523271e5ce
Andrew Black <andrew@black1.org.uk>
2024-05-01 19:10:56 +0100

    remove diags and select min

--- print_pages_gen.rb
+++ print_pages_gen.rb
@@ -1,18 +1,25 @@
 #!/usr/bin/ruby
+if ARGV.count < 3
+  puts <<EOS
 
+  print_pages_gen  file  start end
+  doesnt read file 
+  Outputs a list of chunks
+
+EOS
+   exit
+end 
 
 file = ARGV.shift
 start_page = ARGV.shift.to_i
 finish_page = ARGV.shift.to_i
 ## TODO: check arg (raise error)
 chuncks = 10   ## TODO: or param
+prog = 'print_pages.rb'
 
 range = (start_page .. finish_page)
-puts range
-puts start_page
-puts range.first
-puts range.last
 range.step(chuncks).each do |st|
-  actual_last = st+chuncks - 1  #TODO max
-  puts "pp #{file} #{st} #{actual_last} $"
+  actual_last = [ st+chuncks - 1, finish_page ] .min  
+  puts "# #{prog} #{file} #{st} #{actual_last} "
+  #put "pause" TODO wrong verb
 end
