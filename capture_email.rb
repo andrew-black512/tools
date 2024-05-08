2de38939ef6060ee63330a6a97502509dcbb2ada
Andrew Black <andrew@black1.org.uk>
2024-05-08 09:58:29 +0100

    Add file

--- capture_email.rb
+++ capture_email.rb
@@ -1,8 +1,17 @@
 #!/usr/bin/ruby
-# TODO:  Ouput to a well known file 
+# TODO:  Make filename date dependent.
+
 require "clipboard"
+loglocation = '/home/andrew/work/mailwork/collect_202305.txt'
 
 # TODO P1 not part 
 password =  ARGV.join('') 
 Clipboard.copy password
+puts
 puts "Clipboard : #{Clipboard.paste}"
+puts
+
+begin
+   file=File.open(loglocation, 'a')
+   file.printf("%s,\n",  password) 
+end
