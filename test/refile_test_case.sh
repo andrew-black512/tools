7b1caaf3c9850c3280c9b5788b2404ac92c9771d
Andrew Black <andrew@black1.org.uk>
2024-06-28 10:27:08 +0100

    new file:   refile_test_case.sh

---
+++ test/refile_test_case.sh
@@ -0,0 +1,8 @@
+# swk 
+touch   3-march-2024.pdf 
+touch   6-june-2024.pdf 
+touch   may-2024.pdf 
+touch   2024.02.18-2024.03.16_-_final.pdf 
+touch   2024.03.17-2024.04.13-1.pdf 
+touch   2024.04.14_-_2024.05.11_1.pdf 
+touch   2024.05.12-2024.06.08_2.pdf 
