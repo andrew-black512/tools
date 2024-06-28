3b3d9209ea0c60d26fbf0a2ac69d6bc79e2617f7
Andrew Black <andrew@black1.org.uk>
2024-06-28 10:43:38 +0100

    delete ..../in contents

--- test/refile_test_case.sh
+++ test/refile_test_case.sh
@@ -1,8 +1,17 @@
 # swk 
+# eventual /home/andrew/work/ref/service/swk/test_case.sh
+
+cd refile/in
+pwd
+rm -v *
+
 touch   3-march-2024.pdf 
 touch   6-june-2024.pdf 
-touch   may-2024.pdf 
+touch   may-2024.pdf
+# Note  - may have been despaced 
 touch   2024.02.18-2024.03.16_-_final.pdf 
 touch   2024.03.17-2024.04.13-1.pdf 
 touch   2024.04.14_-_2024.05.11_1.pdf 
 touch   2024.05.12-2024.06.08_2.pdf 
+echo
+ls -1 
