5d7c0692bea0d257fd16646e6b5506f38f923fb1
Andrew Black <andrew@black1.org.uk>
2024-07-13 21:37:46 +0100

    refactor with sensible name

--- capture_email.rb
+++ capture_email.rb
@@ -5,13 +5,13 @@ require "clipboard"
 loglocation = '/home/andrew/work/mailwork/collect_lt_202405.txt'
 
 # TODO P1 not part 
-password =  ARGV.join('') 
-Clipboard.copy password
+email_address =  ARGV.join('') 
+Clipboard.copy email_address
 puts
 puts "Clipboard : #{Clipboard.paste}"
 puts
 
 begin
    file=File.open(loglocation, 'a')
-   file.printf("%s,\n",  password) 
+   file.printf("%s,\n",  email_address) 
 end
