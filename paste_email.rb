9dfe071e65e2edee3bf488c48a32540d79a1e6ed
Andrew Black <andrew@black1.org.uk>
2024-07-16 17:05:25 +0100

    options stuff

--- paste_email.rb
+++ paste_email.rb
@@ -1,18 +1,44 @@
 #!/usr/bin/ruby
-# TODO:  Make filename date dependent.
 
-require "clipboard"
-loglocation = '/home/andrew/work/mailwork/trans_emails.txt'
+require 'optparse'
+require 'pp'
 
-# TODO P1 not part 
-base = '@andrew.black1.org.uk'
-email_address =  ARGV.join('') + base
-Clipboard.copy email_address
-puts
-puts "Clipboard : #{Clipboard.paste}"
-puts
+
+#---------------------------------------------------------------------------
+options = {
+    :domain => 'andrew.black1.org.uk',
+    :comment => '' ,
+}
+OptionParser.new do |opts|
+   opts.banner = "Usage: paste_email.rb  email with space .... "
+
+   opts.on("-f","--file FILE","Give filenameTODO" ) do |file|
+     puts "Given file"
+     options[:file]=file
+   end
+
+   opts.on("-r", "--reverse", "Reverse direction") do |v|
+       options[:reverse] = v
+   end
+   opts.on("-d","--domain ","Give email domain" ) do |d|
+      puts "Given file"
+      options[:domain]=d
+   end
+
+
+   opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+       options[:verbose] = v
+   end
+   opts.environment
+end.parse!
+
+pp options if options[:verbose]
+#helpandexit if ARGV.count == 0
+
 
 begin
-   file=File.open(loglocation, 'a')
-   file.printf("%s,\n",  email_address) 
+  # work here
+rescue Interrupt  => e
+  puts ''
 end
+
