e9d9ae85e419a27223380b53224e435964cef00b
Andrew Black <andrew@black1.org.uk>
2024-07-16 17:14:07 +0100

    combine option and paste strnads

--- paste_email.rb
+++ paste_email.rb
@@ -2,6 +2,8 @@
 
 require 'optparse'
 require 'pp'
+require "clipboard"
+loglocation = '/home/andrew/work/mailwork/trans_emails.txt'
 
 
 #---------------------------------------------------------------------------
@@ -32,13 +34,20 @@ OptionParser.new do |opts|
    opts.environment
 end.parse!
 
+puts "xx  "
 pp options if options[:verbose]
 #helpandexit if ARGV.count == 0
 
+base = '@' +  options[:domain]
+email_address =  ARGV.join('') + base
+Clipboard.copy email_address
+puts
+puts "Clipboard : #{Clipboard.paste}"
+puts
 
 begin
-  # work here
-rescue Interrupt  => e
-  puts ''
+   file=File.open(loglocation, 'a')
+   file.printf("%s,\n",  email_address) 
 end
+
 
