3e427db3e7807a8aa8a6774bab270561462857ff
Andrew Black <andrew@black1.org.uk>
2024-08-28 11:07:12 +0100

    modified:   get_flickr_mb.py

--- get_flickr_mb.py
+++ get_flickr_mb.py
@@ -2,14 +2,20 @@
 
 
 import flickrapi
+import config
 
-api_key = ""
-api_secret = ""
 extra_params={}
-user_id=''
+user_id='lesleyablack' #'andrewblack' #65535'
 
-flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json',store_token=False)
+#flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json',store_token=False)
+flickr = flickrapi.FlickrAPI(config.API_KEY, config.API_SECRET, format='parsed-json')
 
 photos = flickr.photos.search(user_id=user_id,extras='date_taken,description,geo,tags,machine_tags,url_m',**extra_params)
 for photo in photos['photos']['photo']:
     print(photo['title'])
+    print ("    " + photo['url_m'])
+    print ("    " + photo['tags'])
+    print ("    " + photo['date_taken'])
+    
+    print ()
+    exit
