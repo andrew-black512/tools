bf959b5b8ca1d2e1cb0182ffe5fddad1a86cfb8e
Andrew Black <andrew@black1.org.uk>
2024-08-29 11:28:56 +0100

    remove crud

--- get_flickr_mb.py
+++ get_flickr_mb.py
@@ -6,9 +6,8 @@ import config
 import pprint
 
 extra_params={}
-user_id='lesleyablack' #'andrewblack' #65535'
+user_id='lesleyablack' #'andrewblack'
 
-#flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json',store_token=False)
 flickr = flickrapi.FlickrAPI(config.API_KEY, config.API_SECRET, format='parsed-json')
 date_prev = ''
 photos = flickr.photos.search(user_id=user_id,extras='date_taken,description,geo,tags,machine_tags,url_m',**extra_params)
@@ -24,6 +23,5 @@ for photo in photos['photos']['photo']:
     print(f'  {photo["title"]}' )
     print ("    " + photo['url_m'])
     print ("    " + photo['tags'])
-    ##print (f'    {date}')
     
     print ()
