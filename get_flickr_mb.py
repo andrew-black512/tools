63fcfbb8823014baea32b4052fc203a8a11e2345
Andrew Black <andrew@black1.org.uk>
2024-08-29 11:27:52 +0100

    group by date (textual)

--- get_flickr_mb.py
+++ get_flickr_mb.py
@@ -3,19 +3,27 @@
 
 import flickrapi
 import config
+import pprint
 
 extra_params={}
 user_id='lesleyablack' #'andrewblack' #65535'
 
 #flickr = flickrapi.FlickrAPI(api_key, api_secret, format='parsed-json',store_token=False)
 flickr = flickrapi.FlickrAPI(config.API_KEY, config.API_SECRET, format='parsed-json')
-
+date_prev = ''
 photos = flickr.photos.search(user_id=user_id,extras='date_taken,description,geo,tags,machine_tags,url_m',**extra_params)
 for photo in photos['photos']['photo']:
-    print(photo['title'])
+    date = photo['datetaken'][0:10]
+
+    if date != date_prev:
+        print("------------------")
+        print(date)
+        date_prev = date 
+
+
+    print(f'  {photo["title"]}' )
     print ("    " + photo['url_m'])
     print ("    " + photo['tags'])
-    print ("    " + photo['date_taken'])
+    ##print (f'    {date}')
     
     print ()
-    exit
