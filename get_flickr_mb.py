dee7cbb7e785de81891b63ee24b3fa95f974042d
Andrew Black <andrew@black1.org.uk>
2024-09-01 08:00:21 +0100

    add min_taken_date and tags (exploritory):wq

--- get_flickr_mb.py
+++ get_flickr_mb.py
@@ -6,11 +6,18 @@ import config
 import pprint
 
 extra_params={}
-user_id='lesleyablack' #'andrewblack'
-
+user_id= 'andrewblack' # 'lesleyablack'
+ #
+tags = "tourdesouthwark"
 flickr = flickrapi.FlickrAPI(config.API_KEY, config.API_SECRET, format='parsed-json')
 date_prev = ''
-photos = flickr.photos.search(user_id=user_id,extras='date_taken,description,geo,tags,machine_tags,url_m',**extra_params)
+min_taken_date = "2014-08-29"
+#https://www.flickr.com/services/api/flickr.photos.search.html
+photos = flickr.photos.search(user_id=user_id, \
+                              #tags=tags, \
+                              min_taken_date =min_taken_date, \
+                              extras='date_taken,description,geo,\
+                              tags,machine_tags,url_m',**extra_params)
 for photo in photos['photos']['photo']:
     date = photo['datetaken'][0:10]
 
