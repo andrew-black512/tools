75470ae85cb0182f47e543db320f88497af22c3a
Andrew Black <andrew@black1.org.uk>
2024-10-07 22:57:24 +0100

    add 2 params

--- file_date.py
+++ file_date.py
@@ -3,18 +3,29 @@ import argparse
 import sys
 from datetime import timedelta,datetime
 
+def put_clipboard(txt):
+    return 0
+    
+
+prefix = 'DF' 
 numargs = len(sys.argv)-1
 if numargs >= 1:
     #number of days forward - default today
     earlier = int(sys.argv[1])
+    if numargs >= 2:
+        prefix = sys.argv[2]
 else:
     earlier = 0  ### prefer to say None or nil
+    print("""
+      Please give time offset 0= today, +=future
+          """
+          ) 
+    exit
 now = datetime.now()
-prefix="DF "   # TODO
 
 earlier_date = now + timedelta(earlier)
 et = earlier_date.strftime("DF %d-%m-%Y ")
-#if ???????
+
 
 print (et)
 # TODO - send to clipboard
