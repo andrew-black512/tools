bd44f3e8ef4fd13844be9e0fcc50d23c8b8e469d
Andrew Black <andrew@black1.org.uk>
2025-03-15 11:45:59 +0000

    new file:   tidy_screen.sh

---
+++ tidy_screen.sh
@@ -0,0 +1,31 @@
+/home/andrew/Pictures/Screenshots
+ 2131  zoneify.rb 
+ 2132  zonify.rb 
+ 2133  ls
+ 2134  despace.pl *
+ 2135  ls
+ 2136  zonify.rb *.png
+ 2137  zonify.rb \*.png
+ 2138  cd *15
+ 2139  gno.
+ 2140  xdg.
+ 2141  pwd
+ 2142  cd ..
+ 2143  pwd > tidy_me.sh
+ 2144  despace.pl *
+ 2145  despace.pl *.png
+ 2146  zoneify.rb
+ 2147  zonify.rb
+ 2148  zonify.rb \*.png
+ 2149  history 30 >> tidy_me.sh 
+ 2150  vim tidy_me.sh 
+ 2151  ./
+ 2152  ./tidy
+ 2153  chmod.x tidy_me.sh 
+ 2154  ./tidy_me.sh 
+ 2155  vim tidy_me.sh 
+ 2156  ls
+ 2157  less tidy_me.sh 
+ 2158  pwd >> ~/tools/tidy_screen.sh
+ 2159  pwd >> ~/dev/tools/tidy_screen.sh
+ 2160  history 30  >> ~/dev/tools/tidy_screen.sh
