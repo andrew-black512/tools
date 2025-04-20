23c32f3b8c7c1d68ce9cc0e6dae790b80acd4ac2
Andrew Black <andrew@black1.org.uk>
2025-04-20 17:44:18 +0100

    lots of chnages

--- alpa_jig.py
+++ alpa_jig.py
@@ -1,8 +1,24 @@
+"""
+Takes a file jogclue.txt extracted from PDF file for a 
+jigsaw crossword.
+
+TODO
+- use a better regexp (re module)
+- This adds numbers - make configurable whether to use numbers or letters from file.
+- join lines that dont end in ( )
+
+consider different outputs - clues with lengths
+"""
 import re
 filename = "jigclue.txt"
+
+my_dict = {} 
+   # { 4: [1, 3], 
+   #   8: [2, 8] }
+clue_no = 1
 with open(filename) as clue_file :
     for l in clue_file :
-        print(l) 
+        #print(l) 
         
         
         match= re.search( r"\((.*)\)", l )
@@ -10,6 +26,22 @@ with open(filename) as clue_file :
             lengths = match.group(1)
             size = 0
             for s in lengths.split(',') :
-               print(f"  {s}") 
+               #print(f"  {s}") 
                size += int(s)
-            print (f" {size}")
+            #print (f" {size}")
+            if size in my_dict:
+               my_dict[size].append(clue_no )
+            else:
+               my_dict[size] = [clue_no]
+        clue_no += 1
+print(my_dict)
+ct = 0
+for l in sorted(my_dict):
+    print(l)
+    cluect=0
+    for clue in my_dict[l]:
+        print (f"   ,{clue}" )
+        ct += 1
+        cluect += 1
+    print  (f"   ,ct leng=,{cluect}" )
+print ( f"ct = {ct}")
