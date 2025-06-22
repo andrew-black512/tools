2d1fc319ed2ebdfd35835a0d958111cb446b9821
Andrew Black <andrew@black1.org.uk>
2025-06-22 15:37:42 +0100

    prior to rewrite

--- update_gen.sh
+++ update_gen.sh
@@ -1,10 +1,10 @@
   # TODO add option A
-  SAFE=~/safe/umapbackup/
-  unzip -o /home/andrew/Downloads/dl_2022/umap_backup_complete.zip 
+  SAFE=~/safe/gitbackups/
+  #unzip -o /home/andrew/Downloads/dl_2022/umap_backup_complete.zip 
   mv -v --backup=numbered \
-    /home/andrew/Downloads/dl_2022/umap_backup_complete.zip  -t $SAFE
-  ls $SAFE
+    /home/andrew/Downloads/dl_2022/$FILE  -t $SAFE
+  ls $SAFE -lrt
   
   git status
-  git add *.umap
+  git add $FILE
   git commit
