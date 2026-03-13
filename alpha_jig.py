9c3c8a8b12f9519c0adaef0a3e66a185a67d8e38
Andrew Black <andrew@black1.org.uk>
2026-03-13 14:37:17 +0000

    Update

--- alpha_jig.py
+++ alpha_jig.py
@@ -13,18 +13,24 @@ import re
 filename = "jigclue.txt"
 
 my_dict = {} 
+   # Stores the clues for each length
    # { 4: [1, 3], 
    #   8: [2, 8] }
 clue_no = 1
+def find_bracket(l):
+    match= re.search( r"\((.*)\)", l )
+    return match
+
 with open(filename) as clue_file :
     for l in clue_file :
         #print(l) 
         
         
-        match= re.search( r"\((.*)\)", l )
+        match = find_bracket(l)
         if match :
             lengths = match.group(1)
             size = 0
+            # CLUe might be "4,5" and we need 14
             for s in lengths.split(',') :
                #print(f"  {s}") 
                size += int(s)
@@ -38,10 +44,10 @@ print(my_dict)
 ct = 0
 for l in sorted(my_dict):
     print(l)
-    cluect=0
-    for clue in my_dict[l]:
+    clue_list = my_dict[l]
+    for clue in clue_list:
         print (f"   ,{clue}" )
         ct += 1
-        cluect += 1
-    print  (f"   ,ct leng=,{cluect}" )
+    count_this_len = count(clue_list)
+    print  (f"   ,ct leng=,{count_this_len}" )
 print ( f"ct = {ct}")
